910bcc7f5f1e3f0dd9d066a896cf8b4a2b0b2e46
team-iot-fkekk <team.iot.fkekk@gmail.com>
2020-03-26 07:37:46 +0800

    initial commit

---
+++ src/secrets-example.h
@@ -0,0 +1,7 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "YOUR_SSID"	// replace MySSID with your WiFi network name
+#define SECRET_PASS "YOUR_PASSWORD"	// replace MyPassword with your WiFi password
+
+#define SECRET_AUTH_KEY "YOUR BLYNK'S DEVICE TOKEN"  // replace 0000000 with your channel number
